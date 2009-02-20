10dd220c62426db4f352b176a0fc179fcda6c939
Gabriel Gilini <gabriel@usosim.com.br>
2009-02-20 20:25:23 -0300

    Changed the drag object to a Drag constructor, thus allowing multiple drag instances

--- drag.js
+++ drag.js
@@ -21,129 +21,19 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
 
-var drag = {
-    isDragging: false,
-    init: function(elm, handler){
-        this.draggable = (typeof elm == 'string')?
-            document.getElementById(elm):
-            elm;
-        this.handler = (typeof handler == 'undefined')?
-            elm:
-            (typeof handler == 'string')?
-                document.getElementById(handler):
-                handler;
+function Drag(elm, handler){
+    this.isDragging = false;
+    this.draggable = (typeof elm == 'string')?
+        document.getElementById(elm):
+        elm;
+    this.handler = (typeof handler == 'undefined')?
+        this.draggable:
+        (typeof handler == 'string')?
+            document.getElementById(handler):
+            handler;
+    // attach the event callbacks
 
-        // attach the event callbacks
-        this.evtObserve('mousemove', this.watchMouse);
-        this.evtObserve('mousedown', this.initDrag, this.handler);
-        this.evtObserve('mouseup', this.endDrag);
-
-        // prevent text selection on the handler
-        // moz
-        this.handler.onmousedown = function(){return false;}
-        // ie
-        this.handler.onselectstart = function(){return false;}
-
-        // viewport stuff
-        // from jibbering FAQ <http://www.jibbering.com/faq/#getWindowSize>
-        var docEl = document.documentElement;
-        this.IS_BODY_ACTING_ROOT = docEl && docEl.clientHeight === 0;
-        this.IS_DOCUMENT_ELEMENT_HEIGHT_OFF = this.isDocumentElementHeightOff();
-        // setupping the dimensions function
-        // because it is unreliable before body load
-        this.getWinDimensions = this.getWinDimensions();
-    },
-    isDocumentElementHeightOff: function(){
-        var d = document,
-            div = d.createElement('div');
-        div.style.height = "2500px";
-        d.body.insertBefore(div, d.body.firstChild);
-        var r = d.documentElement.clientHeight > 2400;
-        d.body.removeChild(div);
-        return r;
-    },
-    getWinDimensions: function(){
-        if(typeof document.clientWidth == "number") {
-            return function(){
-                return [document.clientWidth, document.clientHeight];
-            }
-        }
-        else if(this.IS_BODY_ACTING_ROOT || this.IS_DOCUMENT_ELEMENT_HEIGHT_OFF) {
-            return function(){
-                return [document.body.clientWidth, document.body.clientHeight];
-            }
-        } else {
-            return function(){
-                return [document.documentElement.clientWidth, document.documentElement.clientHeight];
-            }
-        }
-    },
-    getOffsets: function(){
-        var offLeft =
-            offTop = 0;
-        var elm = this.draggable;
-        if(elm.offsetParent){
-            do{
-                offLeft += elm.offsetLeft;
-                offTop += elm.offsetTop;
-            }while(elm = elm.offsetParent)
-        }
-        return [offLeft, offTop];
-    },
-    _drag: function(){
-        drag.newLeft = parseInt(drag.draggable.style.left, 10) -
-            (drag.lastMouseCoords[0] - drag.mouseCoords[0]);
-        drag.newTop = parseInt(drag.draggable.style.top, 10) -
-            (drag.lastMouseCoords[1] - drag.mouseCoords[1]);
-
-        if(drag.newLeft < 0)
-            drag.draggable.style.left = 0 + 'px';
-        else
-            drag.draggable.style.left = drag.newLeft + 'px';
-
-        if(drag.newTop < 0)
-            drag.draggable.style.top = 0 + 'px';
-        else
-            drag.draggable.style.top = drag.newTop + 'px';
-
-        drag.lastMouseCoords = drag.mouseCoords;
-
-        if(drag.isDragging){
-            setTimeout(drag._drag, 10);
-        }
-    },
-    initDrag: function(){
-        drag.isDragging = true;
-        drag.lastMouseCoords = [
-            drag.mouseCoords[0],
-            drag.mouseCoords[1]
-        ];
-        if(!drag.draggable.style.left || !drag.draggable.style.top){
-            var offsets = drag.getOffsets();
-            drag.draggable.style.left = offsets[0] + 'px';
-            drag.draggable.style.top = offsets[1] + 'px';
-        }
-        drag.windowDimensions = drag.getWinDimensions();
-        drag._drag();
-    },
-    endDrag: function(){
-        drag.isDragging = false;
-    },
-    watchMouse: function(e){
-        e = e || window.event;
-
-        drag.mouseCoords = [
-            e.pageX ||
-            e.clientX +
-                document.body.scrollLeft +
-                document.documentElement.scrollLeft,
-            e.pageY ||
-            e.clientY +
-                document.body.scrollTop +
-                document.documentElement.scrollTop
-        ];
-    },
-    evtObserve: (function(){
+    this.evtObserve = (function(){
         if(document.addEventListener){
             return function(evt, callbackFn, elm){
                 elm = (typeof elm == 'undefined')?document:elm;
@@ -163,4 +53,138 @@ var drag = {
             }
         }
     })()
-};
+    // prevent text selection on the handler
+    // moz
+    this.handler.onmousedown = function(){return false;}
+    // ie
+    this.handler.onselectstart = function(){return false;}
+
+    // viewport stuff
+    // from jibbering FAQ <http://www.jibbering.com/faq/#getWindowSize>
+    var docEl = document.documentElement;
+    this.IS_BODY_ACTING_ROOT = docEl && docEl.clientHeight === 0;
+    this.IS_DOCUMENT_ELEMENT_HEIGHT_OFF = (function(){
+        var d = document,
+            div = d.createElement('div');
+        div.style.height = "2500px";
+        d.body.insertBefore(div, d.body.firstChild);
+        var r = d.documentElement.clientHeight > 2400;
+        d.body.removeChild(div);
+        return r;
+    })();
+    // setupping the dimensions function
+    // because it is unreliable before body load
+    this.getWinDimensions = (function(){
+        if(typeof document.clientWidth == "number") {
+            return function(){
+                return [document.clientWidth, document.clientHeight];
+            }
+        }
+        else if(this.IS_BODY_ACTING_ROOT || this.IS_DOCUMENT_ELEMENT_HEIGHT_OFF) {
+            return function(){
+                return [document.body.clientWidth, document.body.clientHeight];
+            }
+        } else {
+            return function(){
+                return [document.documentElement.clientWidth, document.documentElement.clientHeight];
+            }
+        }
+    })();
+
+    this.getOffsets = function(){
+        var offLeft =
+            offTop = 0;
+        var elm = this.draggable;
+        if(elm.offsetParent){
+            do{
+                offLeft += elm.offsetLeft;
+                offTop += elm.offsetTop;
+            }while(elm = elm.offsetParent)
+        }
+        return [offLeft, offTop];
+    }
+
+    this._drag = function(){
+        this.newLeft = parseInt(this.draggable.style.left, 10) -
+            (this.lastMouseCoords[0] - this.mouseCoords[0]);
+        this.newTop = parseInt(this.draggable.style.top, 10) -
+            (this.lastMouseCoords[1] - this.mouseCoords[1]);
+
+        if(this.newLeft < 0)
+            this.draggable.style.left = 0 + 'px';
+        else
+            this.draggable.style.left = this.newLeft + 'px';
+
+        if(this.newTop < 0)
+            this.draggable.style.top = 0 + 'px';
+        else
+            this.draggable.style.top = this.newTop + 'px';
+
+        this.lastMouseCoords = this.mouseCoords;
+
+        if(this.isDragging){
+            setTimeout(callback(this._drag, {bind: this}), 10);
+        }
+    }
+
+    this.initDrag = function(){
+        console.log(this);
+        this.isDragging = true;
+        this.lastMouseCoords = [
+            this.mouseCoords[0],
+            this.mouseCoords[1]
+        ];
+        if(!this.draggable.style.left || !this.draggable.style.top){
+            var offsets = this.getOffsets();
+            this.draggable.style.left = offsets[0] + 'px';
+            this.draggable.style.top = offsets[1] + 'px';
+        }
+        this.windowDimensions = this.getWinDimensions();
+        this._drag();
+    }
+
+    this.endDrag = function(){
+        this.isDragging = false;
+    }
+
+    this.watchMouse = function(e){
+        e = e || window.event;
+
+        this.mouseCoords = [
+            e.pageX ||
+            e.clientX +
+                document.body.scrollLeft +
+                document.documentElement.scrollLeft,
+            e.pageY ||
+            e.clientY +
+                document.body.scrollTop +
+                document.documentElement.scrollTop
+        ];
+    }
+
+    this.evtObserve('mousemove', callback(this.watchMouse, {bind: this}));
+    this.evtObserve('mousedown', callback(this.initDrag, {bind: this}), this.handler);
+    this.evtObserve('mouseup', callback(this.endDrag, {bind: this}));
+}
+
+// Adapted from http://onemarco.com/2008/11/12/callbacks-and-binding-and-callback-arguments-and-references/
+function callback(func,opts){
+    opts = opts || {};
+    var cb = function(){
+        var args = opts.args ? opts.args : [];
+        var bind = opts.bind ? opts.bind : this;
+        var fargs = opts.supressArgs === true ?
+            [] : toArray(arguments);
+
+        func.apply(bind,fargs.concat(args));
+    }
+    return cb;
+}
+
+function toArray(arrayLike){
+    var arr = [];
+    for(var i = 0; i < arrayLike.length; i++){
+        arr.push(arrayLike[i]);
+    }
+    return arr;
+}
